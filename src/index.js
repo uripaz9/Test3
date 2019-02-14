6fa593be23cef662375982465da0f6be6b83298f
uri paz <uripaz9@gmail.com>
2019-02-14 15:24:16 +0200

    hhgh

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-      <h1>gggg</h1>
+      <h1>ggggghhgghgh</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
